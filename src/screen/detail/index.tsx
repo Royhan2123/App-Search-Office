fa247b5aaf6c963371588b4f52c53e08c8b9b464
Royhan <royhankeren12@gmail.com>
2024-09-08 21:15:36 +0700

    detail screen -done

--- src/screen/detail/index.tsx
+++ src/screen/detail/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import {FlatList, ScrollView, StyleSheet, View} from 'react-native';
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {colors} from '../../../assets/style/Colors';
 import Header from '../../components/Header';
 import SliderItem from '../../components/SliderItem';
+import {Gs} from '../../../assets/style/GlobalStyle';
+import { useNavigation } from '@react-navigation/native';
 
 const DetailScreen = () => {
+
+  const navigation = useNavigation();
+
   const slider = [
     require('../../../assets/images/item_2_a.png'),
     require('../../../assets/images/item_2_b.png'),
@@ -23,6 +36,83 @@ const DetailScreen = () => {
     );
   };
 
+  const renderTitle = () => {
+    return (
+      <View style={style.titleContainer}>
+        <View>
+          <Text style={[Gs.textBlack, Gs.h1]}>Homemade Office</Text>
+          <Text style={[Gs.textGrey]}>Jalan Angga Bekerja No.10</Text>
+        </View>
+        <View style={Gs.flexRow}>
+          <Image source={require('../../../assets/icons/star_yellow.png')} />
+          <Text style={[Gs.textBlack, Gs.h3]}> 4.5/5</Text>
+        </View>
+      </View>
+    );
+  };
+
+  const renderDescription = () => {
+    return (
+      <View style={style.descriptionContainer}>
+        <Text style={[Gs.textBlack, Gs.h3, style.title]}>About</Text>
+        <Text style={[Gs.textGrey]}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book
+        </Text>
+      </View>
+    );
+  };
+
+  const renderOwner = () => {
+    return (
+      <View style={style.ownerContainer}>
+        <Text style={[Gs.textBlack, Gs.h3, style.title]}>Space Owner</Text>
+        <View style={[Gs.flexRow, Gs.itemsCenter]}>
+          <Image
+            source={require('../../../assets/images/profile_2.png')}
+            style={style.ownerImage}
+          />
+          <View>
+            <View style={Gs.flexRow}>
+              <Text>JuneBug</Text>
+              <Image
+                source={require('../../../assets/icons/verified_orange.png')}
+                style={style.icon}
+              />
+            </View>
+            <Text style={[Gs.font700, Gs.textBlack]}>@junebug</Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  const renderBookingButton = () => {
+    return (
+      <View style={style.bookingButtonContainer}>
+        <View>
+          <Text style={[
+            Gs.textPrimary,
+            Gs.h1,
+          ]}>$5,899</Text>
+          <Text style={
+            Gs.textGrey
+          }>/day</Text>
+        </View>
+        <TouchableOpacity style={style.button} onPress={() => {
+          navigation.navigate('Booking');
+        }}>
+          <Text style={Gs.textWhite}>Start Booking</Text>
+          <Image
+            source={require('../../../assets/icons/arrow_right_white.png')}
+            style={style.icon}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
   return (
     <View style={style.container}>
       <Header
@@ -32,11 +122,11 @@ const DetailScreen = () => {
       />
       <ScrollView>
         {renderSLider()}
-        {/* // {renderTitler()}
-        // {renderDescription()}
-        // {renderOwner()}} */}
+        {renderTitle()}
+        {renderDescription()}
+        {renderOwner()}
       </ScrollView>
-      {/* {renderBookingButton()} */}
+      {renderBookingButton()}
     </View>
   );
 };
@@ -45,6 +135,43 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
+  },
+  titleContainer: {
+    ...Gs.flexRow,
+    ...Gs.itemsCenter,
+    ...Gs.justifyBetween,
+    marginTop: 24,
+    paddingHorizontal: 24,
+  },
+  title: {
+    marginBottom: 10,
+  },
+  descriptionContainer: {
+    marginTop: 24,
+    paddingHorizontal: 24,
+  },
+  ownerContainer: {
+    marginTop: 24,
+    paddingHorizontal: 24,
+    marginBottom: 20,
+  },
+  ownerImage: {
+    marginRight: 12,
+  },
+  icon: {
+    marginLeft: 4,
+  },
+  bookingButtonContainer: {
+    ...Gs.flexRow,
+    ...Gs.justifyBetween,
+    padding: 24,
+  },
+  button: {
+    ...Gs.flexRow,
+    ...Gs.cornerMD,
+    paddingHorizontal: 72,
+    paddingVertical: 14,
+    backgroundColor: colors.primary,
   },
 });
 
